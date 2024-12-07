3ce484fd683a7224d84b6e1dd77fc454232f6d3a
Ayan Ansari <mayan9917@gmail.com>
2024-12-07 15:27:40 +0530

    changed db credentials

--- config/database.js
+++ config/database.js
@@ -7,8 +7,13 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", false)
+      ssl: env.bool("DATABASE_SSL", true) && {
+        // rejectUnauthorized: env.bool(
+        //   "DATABASE_SSL_REJECT_UNAUTHORIZED",
+        //   true
+        // ),
+      },
     },
-    debug: false,
-  },    
+     debug: false,
+  },
 });
